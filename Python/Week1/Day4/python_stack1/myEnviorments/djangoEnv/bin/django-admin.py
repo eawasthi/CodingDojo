d5fd67d14a75b6faa424557bf0eb9ddf6b932868
Ekta Awasthi <ekta.awasthi@hotmail.com>
2017-04-17 10:53:32 -0700

    Final Commit of Week1

---
+++ Python/Week1/Day4/python_stack1/myEnviorments/djangoEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ektaawasthi/Desktop/Dojo/Python/Week1/Day4/python_stack1/myEnviorments/djangoEnv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
